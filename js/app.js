6739ecb0abdddcc071a97f93b054166968b19a1c
ztw <zhangtingwei526@foxmail.com>
2017-12-11 12:54:13 +0800

    删除功能，清空已完成项目

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ angular.module('app',[])
           //清空输入框
           $scope.task= ""
         }
-      }
+      }  
   }
 
   // 设置任务是否完成
@@ -25,7 +25,45 @@ angular.module('app',[])
         }
     }
 
+   
+
     return nums
+  }
+
+  // 单条删除任务
+  $scope.delTask = function (index){
+    $scope.taskList.splice(index)
+  }
+
+  //清空已完成
+  $scope.clearCompl = function (item) {
+    // 正向遍历减一
+    // for (let i = 0; i < $scope.taskList.length; i++) {
+    //   if ($scope.taskList[i].isCompleted) {
+    //     $scope.taskList.splice(i,1)
+    //     i--
+    //   }
+    // }
+    // 倒着遍历  
+    for (var i = $scope.taskList.length-1; i >= 0; i--) {
+      if ($scope.taskList[i].isCompleted) {
+        $scope.taskList.splice(i,1)
+       
+      }
+    }
+  }
+
+  //设置编辑状态为false
+  $scope.taskList.push({
+    isEditing:false
+  })
+  //编辑
+  $scope.edit = function (item){
+      item.isEditing = true
+  }
+  //保存
+  $scope.saveTask = function (item) {
+    item.isEditing = false
   }
 
 }])
